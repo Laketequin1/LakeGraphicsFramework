bd4baa3535590740429405fdc68d147aaca6133c
Laketequin1 <Laketequin1@gmail.com>
2025-11-17 12:54:54 +1300

    Added comment, improved

--- safe_file_readlines.py
+++ safe_file_readlines.py
@@ -1,8 +1,16 @@
-def safe_file_readlines(path):
+def safe_file_readlines(file_path: str):
+    """
+    Reads lines from the file, raising errors if there is a failure.
+
+    Will raise an exception if the file is not found or is not readable (E.G. Insufficient permissions)
+
+    Parameters:
+        file_path (str): The path of the file to read.
+    """
     try:
-        with open(path, 'r') as f:
+        with open(file_path, 'r') as f:
             return f.read()
     except FileNotFoundError as e:
-        raise Exception(f"Error: File not found at '{path}'. \n\nError: {e}")
+        raise FileNotFoundError(f"Error: File not found at '{file_path}'. \n\nError: {e}")
     except IOError as e:
-        raise Exception(f"Error: Unable to read file '{path}': {e}")
+        raise IOError(f"Error: Unable to read file '{file_path}': \n\n Error: {e}")
